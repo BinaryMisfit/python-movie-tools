60b8963951b92a4a40c549f13f37d0033ce70562
Willie Roberts <wroberts@mrpg.com>
2019-08-01 14:25:13 +0200

    V3 MovieQualityCheck

--- v3/apps/moviequalitycheck.py
+++ v3/apps/moviequalitycheck.py
@@ -9,3 +9,65 @@
 
 def read_mkv_file(source_file):
     """Retrieve MKV file information"""
+    import sys
+    import collections
+    import enzyme
+    from enzyme.exceptions import MalformedMKVError
+    result = collections.namedtuple('Result', 'mkv_file error')
+    with open(source_file, 'rb') as mkv_source:
+        try:
+            mkv_file = enzyme.MKV(mkv_source)
+        except MalformedMKVError:
+            return result(None, '[ERROR] %s' % sys.exc_info()[0])
+
+    return result(mkv_file, None)
+
+
+def get_media_info(source_file):
+    """Retrieves the media file information"""
+    from pymediainfo import MediaInfo
+    media_info = MediaInfo.parse(source_file)
+    return media_info
+
+
+def get_video_quality(media_info):
+    """Returns the closest matching video quality"""
+    return_quality = "Unknown"
+    for track in media_info.tracks:
+        if track.track_type == "Video":
+            if int(track.sampled_width) == 1920:
+                return_quality = "1080p"
+            elif int(track.sampled_width) == 1280:
+                return_quality = "720p"
+            if int(track.sampled_height) >= 1000:
+                return_quality = "Bluray-" + return_quality
+            elif int(track.sampled_height) >= 800:
+                return_quality = "HDTV-" + return_quality
+            elif int(track.sampled_height) >= 500:
+                return_quality = "Bluray-" + return_quality
+            else:
+                return_quality = "HDTV-" + return_quality
+    return return_quality
+
+
+def main():
+    """Main script interface for Movie Quality Check"""
+    import sys
+    import argparse
+    print('Starting Movie Quality Check')
+    parser = argparse.ArgumentParser(
+        description='Checks a MKV file for video quality')
+    parser.add_argument('file', metavar='file', type=str,
+                        help='File to be checked')
+    args = parser.parse_args()
+    source_file = args.file
+    detail = get_media_info(source_file)
+    quality = get_video_quality(detail)
+    print("Movie quality is " + quality)
+    if "Unknown" not in quality:
+        sys.exit(0)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
