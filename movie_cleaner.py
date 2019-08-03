051f9f927f9a5832e98d30d20b49e2c10dde12ee
Willie Roberts <diagoza@me.com>
2019-08-03 21:32:45 +0200

    Added validate_mkv to movie_cleaner

--- movie_cleaner.py
+++ movie_cleaner.py
@@ -1,22 +1,40 @@
+#!/usr/bin/env python
+
 ##########################################################################
-# Movie Cleaner 
+# Movie Cleaner
 # ###
 
-# Script to clean movie files and folders 
+# Script to clean movie files and folders
 
 # Current Version: 0.0.1
 ##########################################################################
 
 
 class MovieFolder(object):
-    def __init__(self, mkv_file):
+    """Object containing all information for the script"""
+
+    def __init__(self, mkv_file, result='', error=False):
         self.mkv_file = mkv_file
+        self.result = result
+        self.error = error
 
     def __repr__(self):
-        return '<MovieFolder mkv_file: %s' % self.mkv_file
+        return '<MovieFolder mkv_file: %s, result = %s, error = %r>' % \
+            self.mkv_file, self.result, self.error
 
     def __str__(self):
         return repr(self)
+
+
+def validate_mkv(file):
+    """Check if the MKV file exists and populate MovieFolder"""
+    from pathlib import Path
+    mkv_file = Path(file)
+    movie_folder = MovieFolder(mkv_file)
+    if not mkv_file.exists():
+        movie_folder.result = "File not found"
+        movie_folder.error = True
+        return movie_folder
 
 
 def main():
@@ -29,7 +47,13 @@ def main():
     parser.add_argument('file', metavar='file', type=str,
                         help='MKV file to clean')
     args = parser.parse_args()
-    print 'Checking file %s' % args.file
+    print('MKV File\t\t%s' % args.file)
+    movie = validate_mkv(args.file)
+    if movie.error:
+        print('Validation:\t\t%s' % movie.result)
+    else:
+        print('Validation:\t\tSuccess')
+
     output = 0
     sys.exit(output)
 
