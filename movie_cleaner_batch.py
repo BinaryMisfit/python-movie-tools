cbab86ed3899038cf0ccbe76be477d054bb56429
Willie Roberts <diagoza@me.com>
2019-08-06 22:44:02 +0200

    Moved processing message in movie_cleaner_batch

--- movie_cleaner_batch.py
+++ movie_cleaner_batch.py
@@ -48,19 +48,20 @@ def process(folder):
     print('Checking:\t\t{0}').format(batch.folder_name)
     print(colored("==============================================="
                   "===============================================",
+                  "=====================",
                   'blue'))
+    print('Processing:\t\t{0}').format(get_file.data.name)
     for batch_child in batch_folder.iterdir():
         process_file = True
         get_file = get_first_file(str(batch_child), '*.mkv')
-
         if not get_file.result:
             process_file = False
 
         if process_file:
-            print('Processing:\t\t{0}').format(get_file.data.name)
             result = process_folder(str(get_file.data))
             print(colored("==============================================="
                           "===============================================",
+                          "=====================",
                           'blue'))
             if result.error:
                 batch.error = result.error
