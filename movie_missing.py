1ea46cf9a4bd611c773c1558633d56a4190cdf5f
Willie Roberts <diagoza@me.com>
2019-08-12 22:53:52 +0200

    Refactored and movie lookup

--- movie_missing.py
+++ movie_missing.py
@@ -24,6 +24,18 @@ def load_radarr_movies():
     return None
 
 
+def check_movie(movie_list, movie):
+    print(str(movie))
+    mkv_files = []
+    mkv_files = mkv_files.extend(movie.glob('*.mkv'))
+    has_movie = mkv_files is not None and len(mkv_files) > 0
+    movie_info = next((x for x in movie_list if x.path == str(movie)), None)
+    print(movie_info)
+    if has_movie:
+        return 1
+    return 0
+
+
 def main():
     import sys
     from pathlib import Path
@@ -41,12 +53,7 @@ def main():
         if movie_folder.is_dir():
             for movie in movie_folder.iterdir():
                 if movie.is_dir():
-                    mkv_files = []
-                    mkv_files = mkv_files.extend(movie_folder.glob('*.mkv'))
-                    has_movie = mkv_files is not None and len(mkv_files) > 0
-                    if has_movie:
-                        movie_count += 1
-                    print(str(movie))
+                    movie_count += check_movie(movie_list, movie)
 
     print(("Total movies found: {0}").format(movie_count))
     sys.exit(0)
