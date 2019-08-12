7a6597287929fe6550cc3ffb0a99aed1015f4f33
Willie Roberts <diagoza@me.com>
2019-08-12 21:24:39 +0200

    Added Movie Count

--- movie_missing.py
+++ movie_missing.py
@@ -34,13 +34,16 @@ def main():
         sys.exit(1)
 
     movie_folder = Path('/Volumes/ProRaid/movies/')
+    movie_count = 0
     if movie_folder.exists:
         if movie_folder.is_dir:
             for movie in movie_folder.iterdir():
                 if movie.is_dir:
+                    movie += 1
                     if str(movie) not in movie_list:
                         print(str(movie))
 
+    print("Total movies found: {0}").format(movie_count)
     sys.exit(0)
 
 
