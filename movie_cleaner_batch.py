eded770c8326eb69099b19f289551a50e9610321
Willie Roberts <diagoza@me.com>
2019-08-17 13:28:36 +0200

    Change movie_cleaner_batch string handling to fstrings

--- movie_cleaner_batch.py
+++ movie_cleaner_batch.py
@@ -14,15 +14,14 @@ from pathlib import Path
 
 class BatchFolder(object):
     """Object containing all information for the script"""
-    def __init__(self, folder_name, result='', error=False):
+    def __init__(self, folder_name, result="", error=False):
         self.folder_name = folder_name
         self.result = result
         self.error = error
 
     def __repr__(self):
-        return '<BatchFolder folder_name: {3}, result: {8}, ' \
-            ' error: {9}>'.format(self.folder_name, self.result,
-                                  self.error)
+        return f"<BatchFolder folder_name: {self.folder_name}, result:"
+        f"{self.result}, error: {self.error}"
 
     def __str__(self):
         return repr(self)
@@ -35,24 +34,24 @@ def process(folder):
     batch_folder = Path(batch.folder_name)
     if not batch_folder.exists():
         batch.error = True
-        batch.result = 'Folder not found'
+        batch.result = "Folder not found"
         return batch
 
     if not batch_folder.is_dir():
         batch.error = True
-        batch.result = 'Not a folder'
+        batch.result = "Not a folder"
         return batch
 
-    print('Checking:\t\t{0}').format(batch.folder_name)
+    print(f"Checking:\t\t{batch_folder.name}")
     for batch_child in batch_folder.iterdir():
         process_file = True
         if not batch_child.is_dir():
             process_file = False
 
         if process_file:
-            print('---')
-            print('Processing:\t\t{0}').format(batch_child.name)
-            get_file = get_first_file(str(batch_child), '*.mkv')
+            print("---")
+            print(f"Processing:\t\t{batch_child.name}n)
+            get_file = get_first_file(str(batch_child), "*.mkv")
             if not get_file.result:
                 process_file = False
 
@@ -60,7 +59,7 @@ def process(folder):
             result = process_folder(str(get_file.data))
             if result.error:
                 batch.error = result.error
-                batch.result = ("Failed - {0}").format(batch_child.name)
+                batch.result = (f"Failed - {batch_child.name}")
                 break
 
     return batch
@@ -74,19 +73,19 @@ def main():
     import argparse
     init()
     parser = argparse.ArgumentParser(
-        description='Clean all movies stored in specified folder')
-    parser.add_argument('folder',
-                        metavar='folder',
+        description="Clean all movies stored in specified folder")
+    parser.add_argument("folder",
+                        metavar="folder",
                         type=str,
-                        help='Folder to check')
+                        help="Folder to check")
     args = parser.parse_args()
     movie = process(args.folder)
     if movie.error:
-        print('Error\t\t{0}').format(colored(movie.result, 'red'))
+        print(f"Error\t\t{colored(movie_result, "red")}")
         sys.exit(1)
 
     sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
