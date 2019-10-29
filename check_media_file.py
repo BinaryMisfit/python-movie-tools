fb17ddf0f837e90c76a6db5925da4dcaee10af44
Willie Roberts <diagoza@me.com>
2019-10-29 21:58:08 +0000

    Added argparse to check_media_file

--- check_media_file.py
+++ check_media_file.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python2
+###
+# A supplied file will be checked for media type and valid video and launguage # information.
+###
+
+
+def prepare_args():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Check media file information")
+    parser.add_argument(
+        "filename", help="The name of the file to be checked")
+    args = parser.parse_args()
+    print(args.filename)
 
 
 def main():
     import sys
+    prepare_args()
     sys.exit(0)
 
 
