06a420d2df16f1ecf648c21db6923fc86b949972
Willie Roberts <diagoza@me.com>
2019-10-29 20:37:55 +0000

    Added autopep8 to container

--- check_media_file.py
+++ check_media_file.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python2
 
-##########################################################################
-# Check media file details
-# ###
-
-# Return details of a single media file
-
-# Current Version: 0.0.1
-##########################################################################
-
 
 def main():
     import sys
