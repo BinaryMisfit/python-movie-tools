d3b997e84f75f67425e225dc1ba322f8da11345c
Willie Roberts <diagoza@me.com>
2019-10-29 16:53:10 +0200

    Completed .devcontainer with required pip modules

---
+++ check_media_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+##########################################################################
+# Check media file details
+# ###
+
+# Return details of a single media file
+
+# Current Version: 0.0.1
+##########################################################################
+
+
+def main():
+    import sys
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
