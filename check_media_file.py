d986c94fdab806c2c0d44ed6738e08259ccc4225
Willie Roberts <diagoza@me.com>
2019-10-30 15:45:09 +0000

    Created MediaFile class

--- check_media_file.py
+++ check_media_file.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python2
 ###
-# A supplied file will be checked for media type and valid video and launguage # information.
+# A supplied file will be checked for media type and valid video and language
+# information.
 ###
+
+
+class MediaFile(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __repr__(self):
+        return ("<MediaFile filename: {0}").format(self.filename)
+
+    def __str__(self):
+        return repr(self)
 
 
 def prepare_args():
@@ -11,13 +23,14 @@ def prepare_args():
     parser.add_argument(
         "filename", help="The name of the file to be checked")
     args = parser.parse_args()
-    print(args.filename)
+    media_file = MediaFile(filename=args.filename)
+    print(media_file)
 
 
 def main():
-    import sys
+    from sys import exit
     prepare_args()
-    sys.exit(0)
+    exit(0)
 
 
 if __name__ == "__main__":
